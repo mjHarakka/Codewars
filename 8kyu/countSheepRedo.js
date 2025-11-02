b6ab52fd6705cf9cdf1b656acbb304421b05c387
Mikko Harakka <mj.harakka@gmail.com>
2025-11-02 18:54:33 +0200

    done

---
+++ 8kyu/countSheepRedo.js
@@ -0,0 +1,32 @@
+function countSheeps(arr) {
+  return arr.filter(Boolean).length
+}
+
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+)
