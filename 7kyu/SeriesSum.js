3d54527b7065abdc3200601f9fc31c9e88d67ead
Mikko Harakka <mj.harakka@gmail.com>
2026-01-15 16:37:37 +0200

    ai answer

--- 7kyu/SeriesSum.js
+++ 7kyu/SeriesSum.js
@@ -10,4 +10,13 @@ function SeriesSum(n) {
   return result.toFixed(2)
 }
 
-console.log(SeriesSum(2))
+/*
+AI ANSWER1
+function SeriesSum(n) {
+  let sum = 0
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + 3 * i)
+  }
+  return sum.toFixed(2)
+}
+*/
